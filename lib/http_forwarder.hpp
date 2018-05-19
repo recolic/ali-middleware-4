247763e54b93ddfeb9ced8773a6200832254730d
Recolic Keghart <root@recolic.net>
2018-05-19 17:20:37 +0800

    sync deprecated http_forwarder but do not use it.

--- lib/http_forwarder.hpp
+++ lib/http_forwarder.hpp
@@ -5,12 +5,39 @@
 #ifndef ALI_MIDDLEWARE_AGENT_HTTP_FORWARDER_HPP
 #define ALI_MIDDLEWARE_AGENT_HTTP_FORWARDER_HPP
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/asio.hpp>
 
+#include <producer_selector.hpp>
 
-class http_forwarder {
+namespace consumer {
 
-};
+// This http forwarder must be implemented with multi-threading and coroutine.
+    class [[deprecated]] http_forwarder : rlib::noncopyable {
+    public:
+        http_forwarder() = delete;
 
+        http_forwarder(producer_selector &selector) : selector(selector) {}
 
+        [[noreturn]] void serve(const std::string &listen_addr, uint16_t listen_port/*etcd arg?*/) {
+
+        }
+
+    private:
+        boost::asio::io_context io_context;
+        producer_selector selector;
+    };
+
+}
+
+namespace provider {
+    class [[deprecated]] http_dubbo_forwarder {
+    public:
+        [[noreturn]] void serve() {
+
+        }
+    };
+}
 
 #endif //ALI_MIDDLEWARE_AGENT_HTTP_FORWARDER_HPP
