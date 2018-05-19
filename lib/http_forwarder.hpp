5baad466d9f85ae10406d9887b975787be21b3c6
Recolic Keghart <root@recolic.net>
2018-05-19 12:19:09 +0800

    remove submodule `http`

---
+++ lib/http_forwarder.hpp
@@ -0,0 +1,16 @@
+//
+// Created by recolic on 18-5-19.
+//
+
+#ifndef ALI_MIDDLEWARE_AGENT_HTTP_FORWARDER_HPP
+#define ALI_MIDDLEWARE_AGENT_HTTP_FORWARDER_HPP
+
+
+
+class http_forwarder {
+
+};
+
+
+
+#endif //ALI_MIDDLEWARE_AGENT_HTTP_FORWARDER_HPP
