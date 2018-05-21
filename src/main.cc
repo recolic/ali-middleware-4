4063a842214ea389e795aaf0cb63f96e04607402
Recolic Keghart <root@recolic.net>
2018-05-21 21:56:32 +0800

    working on main.cc. fix some compile-time bug.

--- src/main.cc
+++ src/main.cc
@@ -6,28 +6,36 @@
 #include <rlib/stdio.hpp>
 #include <rlib/log.hpp>
 
-rlib::logger rlog(stdout);
+rlib::logger rlog(std::cout);
 
 using rlib::println;
 using namespace rlib::literals;
 
 int main(int argc, char **argv) {
     auto help_and_exit = [&argv]{
-        println("Usage: {} <producer/consumer> [args ...]"_format(argv[0]));
+        println("Usage: {} <producer-*/consumer> [args ...]"_format(argv[0]));
         exit(1);
     };
 
     if(argc == 1)
         help_and_exit();
 
-    std::string whoami = argv[1];
     rlib::opt_parser opt(argc, argv);
-    if(whoami == "producer") {
+    if (opt.getBoolArg("--help", "-h"))
+        help_and_exit();
+
+    auto whoami = opt.getCommand();
+
+    if (whoami.substr(0, 8) == "producer") {
         // TODO: parse and call
 
     }
     else if(whoami == "consumer") {
+        auto listen_addr = opt.getValueArg("--listen", "-l");
+        auto listen_port = opt.getValueArg("--listen-port", "-p").as<uint16_t>();
 
+        auto debug_server_addr = opt.getValueArg("--debug-server-addr", false);
+        auto debug_server_port = opt.getValueArg("--debug-server-port", false, "0").as<uint16_t>();
     }
     else {
         help_and_exit();
