e025d9eca80b63c425b7bab3c397eacf57ccd42e
Yue Pan <zxc479773533@gmail.com>
2018-05-24 00:36:47 +0800

    Understanding lbs's frame and build producer frame, add TODOs

--- src/producer_agent.cc
+++ src/producer_agent.cc
@@ -1,17 +1,53 @@
 //
 // Created by recolic on 18-5-17.
 //
+// zxcpyp started working on 18-5-23.
+//
 
-#include "producer_agent.hpp"
+#include <producer_agent.hpp>
+
+#include <boost/beast/http/error.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/asio/spawn.hpp>
+
+using string_view = boost::beast::string_view;
+namespace http = boost::beast::http;
+
+namespace asio = boost::asio;
+namespace ip = asio::ip;
+using tcp = ip::tcp;
+
+using namespace rlib::literals;
+
 
 namespace producer {
 
-    agent::agent(const std::string &etcd_addr, uint16_t etcd_port)
-            : sockEtcd(io_context) {
+    agent::agent(const std::string &etcd_addr_and_port) {
         // TODO: Connect to etcd, register myself. Launch heartbeat thread.
     }
 
-    void agent::listen(const std::string &listen_addr, uint16_t listen_port) {
+    [[noreturn]] void listen(const std::string &listen_addr, uint16_t listen_port) {
         // TODO: Launch http server, listen consumer request, and make dubbo request(thread or coroutine).
+    }
+
+    void listen_consumer() {
+        // TODO: Launch http server, listen consumer request. If get request, then call sendto_producer().
+    }
+
+    void sendto_producer() {
+        // TODO: Make dubbo request to producer. Then call listen_producer().
+    }
+
+    void listen_producer() {
+        // TODO: Listen consumer request. If get request, then call sendto_consumer().
+    }
+
+    void sendto_consumer() {
+        // TODO: Send HTTP request to consumer.
+    }
+
+    [[noreturn]] void etcd_register_and_heartbeat(const std::string &etcd_addr_and_port) {
+        // TODO: Send heartbeat packet to etcd.
     }
 }
--- src/producer_agent.hpp
+++ src/producer_agent.hpp
@@ -1,25 +1,51 @@
 //
 // Created by recolic on 18-5-17.
 //
+// zxcpyp started working on 18-5-23.
+//
 
 #ifndef ALI_MIDDLEWARE_AGENT_PRODUCER_AGENT_HPP
 #define ALI_MIDDLEWARE_AGENT_PRODUCER_AGENT_HPP
 
 #include <rlib/class_decorator.hpp>
+#include <rlib/log.hpp>
+
 #include <string>
 #include <boost/asio.hpp>
+#include <boost/coroutine2/coroutine.hpp>
+
+#include <boost/asio/spawn.hpp>
+#include <boost/beast/http.hpp>
+
+extern rlib::logger rlog; // definition in src/main.cc
 
 namespace producer {
 
     class agent : rlib::noncopyable {
     public:
         agent() = delete;
+
+        // Connect to etcd and register myself.
         agent(const std::string &etcd_addr_and_port);
         // Launch http server and listen for consumer_agent. Be caution that you should reuse connections.
         [[noreturn]] void listen(const std::string &listen_addr, uint16_t listen_port);
 
     private:
         boost::asio::io_context io_context;
+
+        // TODO:
+        // Four steps:
+        // 1.Consumer Agent => Producer Agent: Receive HTTP (FORM)
+        void listen_consumer();
+
+        // 2.Producer Agent => Producer: Use DUBBO (JSON)
+        void sendto_producer();
+
+        // 3.Producer => Producer Agent: JSON
+        void listen_producer();
+
+        // 4.Producer Agent => Consumer Agent: Send HTTP (TEXT)
+        void sendto_consumer();
 
         // Not sure if etcd do need heartbeat.
         [[noreturn]] void etcd_register_and_heartbeat(const std::string &etcd_addr_and_port);
