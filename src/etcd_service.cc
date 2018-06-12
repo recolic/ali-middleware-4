9fe8a2b7e889bbcb07f0e175df58ee7264b5946f
Alisahhh <923431911@qq.com>
2018-06-12 19:22:45 +0800

    I don't know what i am write...and I can't compire it...It's tooooo hard...

--- src/etcd_service.cc
+++ src/etcd_service.cc
@@ -3,8 +3,12 @@
 //
 
 #include "etcd_service.hpp"
+#include <Client.hpp>
+#include <rpc.grpc.pb.h>
+#include <etcdserver.pb.h>
 
 void etcd_service::connect(const std::string &etcd_addr_and_port) {
+    etcd::Client etcd(etcd_addr_and_port);
     /*
      * do_connect(this->conn, etcd_addr_and_port);
      * // If you're using REST api, you must open http connection and transfer data by boost::beast
@@ -16,16 +20,31 @@ void etcd_service::connect(const std::string &etcd_addr_and_port) {
      */
 }
 
-void etcd_service::append(const etcd_service::key_type &key, const etcd_service::value_type &value) {
+void etcd_service::append(const std::string etcd_addr_and_port ,const etcd_service::key_type &key, const etcd_service::value_type &value) {
     /*
      * Append value to list on etcd server. Do it with conn and boost::asio::write.
      * No performance worry, no need for async.
      *
      * boost::asio::write(conn, something);
      */
+    etcd::Client etcd(etcd_addr_and_port);
+    std::string addr="/test/";
+    addr=addr+std::string(key);
+    std::pplx::task<etcd::Response>response_task=etcd.set(addr,std::string(value));
+    try {
+        etcd::Response response = response_task.get();
+        if (response.is_ok())
+            std::cout << "The new value is successfully set, previous value was "
+                      << response.prev_value().as_string();
+        else
+            std::cout << "operation failed, details: " << response.error_message();
+    }
+    catch (std::ecxeption const & ex) {
+        std::cerr << "communication problem, details: " << ex.what();
+    }
 }
 
-const etcd_service::container_type &etcd_service::get_list(const etcd_service::key_type &key) {
+const etcd_service::container_type &etcd_service::get_list(const std::string etcd_addr_and_port,const etcd_service::key_type &key) {
     /*
      * Query server list from etcd server. Do it with conn and boost::asio::read.
      * No performance worry, no need for async.
@@ -33,5 +52,12 @@ const etcd_service::container_type &etcd_service::get_list(const etcd_service::k
      * boost::asio::read(conn, this->cache);
      * return std::cref(this->cache);
      */
+    container_type server;
+    etcd::Client etcd(etcd_addr_and_port);
+    std::string addr="/test/new_dir";
+    etcd::Response resp = etcd.ls(addr).get();
+    for(int i = 0;i <resp.keys().size();++ i){
+        server.push_back(std::pair{resp.key(),resp.value()});
+    }
     return container_type();
 }
